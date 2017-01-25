ebf7e1a41cc4eb62501d4a9f541a688a4bdc3167
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-01-25 23:16:48 +0100

    simply working example

---
+++ app.js
@@ -0,0 +1,23 @@
+// dependencies
+var express = require('express');
+//var mongoose = require('mongoose');
+
+// global config
+var app = express();
+var PORT = process.env.PORT || 3000;
+var MONGO_DB = process.env.MONGO_DB;
+var BEST_KEPT_SECRET = process.env.BEST_KEPT_SECRET;
+
+app.use('/assets', express.static(__dirname + '/assets'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+/*mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_DB);*/
+
+require('./routes/routes')(app);
+
+// run server
+app.listen(PORT);
---
+++ routes/routes.js
@@ -0,0 +1,23 @@
+module.exports = function (app) {
+
+  app.use(require('body-parser').urlencoded({ extended: true }));
+
+  app.get('/', function (req, res) {
+    res.render('index');
+  });
+
+  app.post('/addsymbol', function(req, res){
+    yahooFinance.historical({
+    symbol: req.body.symbol,
+    from: '2016-01-01',
+    to: '2016-12-31',
+    period: 'm'
+    }, function (err, data) {
+      if (err) throw err;
+      res.send(data);
+    });
+  })
+
+}
+
+var yahooFinance = require('yahoo-finance');
